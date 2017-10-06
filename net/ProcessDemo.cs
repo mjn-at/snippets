e06f1a4a9f05875cea40098c077912173643d8d2
Martin Nagel <31646307+mjn-at@users.noreply.github.com>
2017-10-06 08:50:16 +0200

    Create ProcessDemo.cs

---
+++ net/ProcessDemo.cs
@@ -0,0 +1,27 @@
+using System;using System.Diagnostics;
+class ProcessDemo    
+{
+  static void Main()  
+  {
+    LaunchCommandLineApp();
+  } 
+ static void LaunchCommandLineApp() 
+ {  
+  ProcessStartInfo startInfo = new ProcessStartInfo();  
+  startInfo.CreateNoWindow = false;  
+  startInfo.UseShellExecute = false;  
+  startInfo.FileName = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";  
+  startInfo.Arguments = "-c Write-Host SUCCESS -Fore Green";  
+  try         
+    {            
+      using (Process exeProcess = Process.Start(startInfo))
+      {
+        exeProcess.WaitForExit();
+      }
+    }
+    catch 
+    {
+      Console.WriteLine(startInfo.ToString());
+    }
+  }
+}
